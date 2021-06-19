872b2a737a41cfdb254486c055733e390ca233aa
gabrielgoto <gabriel.goto@gmail.com>
2021-06-19 15:37:32 -0400

    Moved the program file
    To the GitHub directory

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PracticeSet0
  Practice Set 0 from MIT OCW 6.0001 - Prgramming in Python
+
+This is the initial exercise from the MIT OCW Course 6.0001 – Introduction to Computer Science and Programming in Python
---
+++ problemSet0.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 19 15:17:37 2021
+
+@author: PasqualeDeAngelis
+"""
+
+import math
+
+print('Enter number x: ')
+x = int(input())
+print('Enter number y: ')
+y = int(input())
+raised = x**y
+print(f'x**y = {raised}')
+logBase2 = math.log(x, 2)
+print(f'log(x) = {logBase2}')
